cf9ceaca9f5c3d1553677cdc921bff6d1e7635d5
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-06-30 19:44:53 +0530

    feat

--- src/app/dashboard/department/[code]/page.tsx
+++
@@ -1,6 +0,0 @@
-// import ViewPage from "@pages/ViewPage";
-import React from "react";
-
-export default function View() {
-  return <div></div>
-}
--- src/app/dashboard/department/page.tsx
+++
@@ -1,8 +0,0 @@
-import DepartmentPage from '@pages/Departmentpage'
-import React from 'react'
-
-export default function page() {
-  return (
-    <DepartmentPage/>
-  )
-}
